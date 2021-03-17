9868288a65916ec78addb6d6ab54f16f8c46e451
Armando Valderrama <armando.valderrama@e-vanhost.net>
2021-03-17 01:18:43 -0600

    Creacion de local_settings.template para definir la configuracion inicial basica del proyecto.

--- encuestas_hal/settings.py
+++ encuestas_hal/settings.py
@@ -19,14 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k5hmg)3ir+#tbq&60%=6!c^m&@xvzqwm)j2*by89%g*v)lw#r*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,17 +63,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'encuestas_hal.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -118,3 +100,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print(
+        "Missing local settings. Please check out local_settings.template\
+        and adjust it according to your system."
+    )
+    exit()
