d9271dcb73405c4ca47fed6b55331f2fdf7ccca5
Armando Valderrama <armando.valderrama@e-vanhost.net>
2021-03-21 18:18:06 -0600

    Solucion de error en el archivo config

--- encuestas_hal/settings.py
+++ encuestas_hal/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.os.environ.getiron.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
